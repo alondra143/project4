066ef4ca55e426f2bafc65ff300861fe0273cc95
alondra143 <not given>
2021-11-22 13:27:00 -0800

    fix db location

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  "mongodb+srv://alondra143:nikkibby1@cluster0.ii8wy.mongodb.net/project2?retryWrites=true&w=majority", // < replace with your database name!
+  "mongodb+srv://alondra143:nikkibby1@cluster0.ii8wy.mongodb.net/project4?retryWrites=true&w=majority", // < replace with your database name!
 )
 
 const db = mongoose.connection;
